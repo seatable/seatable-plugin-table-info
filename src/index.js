0193207b3ccef8ef36f4ecf822bcc990bf2707e8
renjie-run <rj.aiyayao@gmail.com>
2023-12-01 16:31:00 +0800

    refactor: replaced by dtable-utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DTable from 'dtable-sdk';
 import App from './app';
 import './setting';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TaskList {
 
-window.app = window.app ? window.app : {};
-window.app.onClosePlugin = function() {
+  static async init() {
+    const dtableSDK = new DTable();
+
+    // local develop
+    window.app = {};
+    window.app.state = {};
+    window.dtable = {
+      ...window.dtablePluginConfig,
+    };
+    await dtableSDK.init(window.dtablePluginConfig);
+    await dtableSDK.syncWithServer();
+
+    window.app.collaborators = dtableSDK.dtableStore.collaborators;
+    window.app.collaboratorsCache = {};
+    window.app.state.collaborators = dtableSDK.dtableStore.collaborators;
+    window.dtableWebAPI = dtableSDK.dtableWebAPI;
+    window.app.onClosePlugin = () => {};
+    window.dtableSDK = dtableSDK;
+  }
+
+  static async execute() {
+    await this.init();
+    ReactDOM.render(<App isDevelopment showDialog />, document.getElementById('root'));
+  }
 
 }
+
+TaskList.execute();
 
